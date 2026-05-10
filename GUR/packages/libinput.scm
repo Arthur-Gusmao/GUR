cdbce18190d6b07e252c6af144178cff3de59440
Arthur Gusmao <arthurgusmao@proton.me>
2026-05-10 10:18:05 -0300

    fix: libinput

--- GUR/packages/libinput.scm
+++ GUR/packages/libinput.scm
@@ -1,6 +1,6 @@
 (define-module (GUR packages libinput)
   #:use-module ((gnu packages freedesktop)
-                #:select (libinput)
+                #:select (libinput-minimal)
                 #:prefix upstream:)
   #:use-module (guix packages)
   #:use-module (guix gexp)
@@ -8,21 +8,20 @@
 
 (define-public libinput-custom
   (package
-    (inherit upstream:libinput)
+    (inherit upstream:libinput-minimal)
     (name "libinput-custom")
     (arguments
      (substitute-keyword-arguments
-         (package-arguments upstream:libinput)
+         (package-arguments upstream:libinput-minimal)
        ((#:phases phases)
         #~(modify-phases #$phases
             (add-after 'install 'add-quirks
               (lambda* (#:key outputs #:allow-other-keys)
                 (let* ((out (assoc-ref outputs "out"))
-                       (dir (string-append out "/etc/libinput"))
-                       (file (string-append dir "/libinput-overrides.quirks")))
+                       (dir (string-append out "/share/libinput"))
+                       (file (string-append dir "/99-custom-overrides.quirks")))
                   (mkdir-p dir)
                   (call-with-output-file file
                     (lambda (port)
                       (display "[debounce]\nMatchName=*\nModelBouncingKeys=1\n"
-                               port)))
-                  #t)))))))))
+                               port))))))))))))
