d29133123c461706cff9cd3559948f60a665dabf
Arthur Gusmao <arthurgusmao@proton.me>
2026-05-10 09:58:04 -0300

    fix: another yet fix in the libinput package

--- GUR/packages/libinput.scm
+++ GUR/packages/libinput.scm
@@ -6,37 +6,23 @@
   #:use-module (guix gexp)
   #:use-module (guix utils))
 
-(define-public libinput
+(define-public libinput-custom
   (package
     (inherit upstream:libinput)
-
+    (name "libinput-custom")
     (arguments
      (substitute-keyword-arguments
          (package-arguments upstream:libinput)
-
        ((#:phases phases)
-
         #~(modify-phases #$phases
-
             (add-after 'install 'add-quirks
-
               (lambda* (#:key outputs #:allow-other-keys)
-
                 (let* ((out (assoc-ref outputs "out"))
                        (dir (string-append out "/etc/libinput"))
-                       (file (string-append
-                              dir
-                              "/libinput-overrides.quirks")))
-
+                       (file (string-append dir "/libinput-overrides.quirks")))
                   (mkdir-p dir)
-
                   (call-with-output-file file
                     (lambda (port)
-                      (display
-"[debounce]
-MatchName=*
-ModelBouncingKeys=1
-"
+                      (display "[debounce]\nMatchName=*\nModelBouncingKeys=1\n"
                                port)))
-
                   #t)))))))))
